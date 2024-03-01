43be831bdaa0e91f2b9bde8b14fe41afceedafca
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2024-03-01 14:35:50 +0530

    scrapped azure, using langchain

--- app.py
+++ app.py
@@ -1,42 +1,103 @@
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from flask import Flask
+import streamlit as st
 from dotenv import load_dotenv
-import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template 
+from langchain.llms import HuggingFaceHub
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
-def upload_document_to_blob_storage(file_path, container_name, blob_name, connection_string):
-    try:
-        # Create BlobServiceClient using the connection string
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-        # Get a container client
-        container_client = blob_service_client.get_container_client(container_name)
-
-        # Create a blob client
-        blob_client = container_client.get_blob_client(blob_name)
-
-        # Upload the document to blob storage
-        with open(file_path, "rb") as data:
-            blob_client.upload_blob(data)
-
-        print(f"Document '{blob_name}' uploaded to Azure Blob Storage successfully.")
-
-    except Exception as e:
-        print(f"Error uploading document to Azure Blob Storage: {e}")
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 
-load_dotenv()
-app = Flask(__name__, template_folder="templates")
-
-if __name__ == "__main__":
-    print("Entered main")
-    # Replace these values with your actual Azure Blob Storage information
-    connection_string = os.getenv("CONNECTION_STRING")
-    container_name = os.getenv("CONTAINER_NAME")
-    blob_name = os.getenv("BLOB_NAME")
-    file_path = os.getenv("FILE_PATH")
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
 
-    upload_document_to_blob_storage(file_path, container_name, blob_name, connection_string)
-    app.run(host='0.0.0.0', port = 8085)
-    app.debug = True
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Document Intelligence")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("What does the document tell? :coffee:")
+    user_question = st.text_input("What do you wanna know?")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
+    main()
