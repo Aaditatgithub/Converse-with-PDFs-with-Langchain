f17031bb686f002e4a53fd26c3577fe6e916b041
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2024-03-02 09:29:55 +0530

    image, speech and text works

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 import speech_recognition as sr
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template 
-from langchain.llms import HuggingFaceHub
-
 from gtts import gTTS
 import os
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageProcessor, DetrForObjectDetection
+import torch
 
-# Function to convert audio input to text using SpeechRecognition library
 def speech_to_text():
     r = sr.Recognizer()
     with sr.Microphone(device_index=1) as source:
@@ -29,6 +29,63 @@ def speech_to_text():
     except sr.RequestError as e:
         st.write(f"Could not request results from Google Speech Recognition service; {e}")
         return ""
+    
+
+def get_image_caption(image_path):
+    """
+    Generates a short caption for the provided image.
+    Args:
+        image_path (str): The path to the image file.
+    Returns:
+        str: A string representing the caption for the image.
+    """
+    image = Image.open(image_path).convert('RGB')
+
+    model_name = "Salesforce/blip-image-captioning-large"
+    device = "cpu"  # cuda
+
+    processor = BlipProcessor.from_pretrained(model_name)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
+
+    inputs = processor(image, return_tensors='pt').to(device)
+    output = model.generate(**inputs, max_new_tokens=20)
+
+    caption = processor.decode(output[0], skip_special_tokens=True)
+
+    return caption
+
+
+def detect_objects(image_path):
+    """
+    Detects objects in the provided image.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        str: A string with all the detected objects. Each object as '[x1, x2, y1, y2, class_name, confindence_score]'.
+    """
+    image = Image.open(image_path).convert('RGB')
+
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    detections = ""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        detections += '[{}, {}, {}, {}]'.format(int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+        detections += ' {}'.format(model.config.id2label[int(label)])
+        detections += ' {}\n'.format(float(score))
+
+    return detections
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -94,37 +151,71 @@ def main():
     load_dotenv()
     st.set_page_config(page_title="Document Intelligence")
     st.write(css, unsafe_allow_html=True)
-    
+
+    option = st.sidebar.selectbox("Navigation", ["Document", "Image", "Speech"])
+
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("What does the document tell? :coffee:")
-    
-    if st.button("Click to Speak"):
-        handle_userinput(speech_to_text())
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            print("hello")
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    if option == "Document":
+        st.header("What does the document tell? :coffee:")
+        user_question = st.text_input("What do you wanna know?")
+        if user_question:
+            handle_userinput(user_question)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
+        with st.sidebar:
+            st.subheader("Your documents")
+            pdf_docs = st.file_uploader(
+                "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            if st.button("Process"):
+                with st.spinner("Processing"):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    vectorstore = get_vectorstore(text_chunks)
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
+    elif option == "Image":
+        st.header("Image Processing")
+        uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+        if uploaded_image is not None:
+            st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+            if st.button("Process"):
+                with st.spinner("Processing..."):
+                    # Save the uploaded image to a temporary location
+                    image_path = "temp_image.jpg"
+                    with open(image_path, "wb") as f:
+                        f.write(uploaded_image.read())
 
+                    # Detect objects
+                    detections = detect_objects(image_path)
+                    st.subheader("Detected Objects")
+                    st.text(detections)
+
+                    # Generate image caption
+                    caption = get_image_caption(image_path)
+                    st.subheader("Image Caption")
+                    st.write(caption)
+                    text_to_speech(caption)
+
+
+    elif option == "Speech":
+        st.header("Ask a question using speech")
+        if st.button("Click to Speak"):
+            handle_userinput(speech_to_text())
+        with st.sidebar:
+            st.subheader("Your documents")
+            pdf_docs = st.file_uploader(
+                "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            if st.button("Process"):
+                with st.spinner("Processing"):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    vectorstore = get_vectorstore(text_chunks)
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
 
 if __name__ == '__main__':
     main()
